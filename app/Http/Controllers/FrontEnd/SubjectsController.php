e6861b717aa77f156ccc7bf999fe252000b18c11
Ana Beatriz Torrez Gomez <anitatorrez1924@gmail.com>
2021-01-17 20:17:17 -0600

    hola

--- app/Http/Controllers/FrontEnd/SubjectsController.php
+++ app/Http/Controllers/FrontEnd/SubjectsController.php
@@ -100,4 +100,32 @@ class SubjectsController extends Controller
         return $result;
     }
 
+    public function getLocation(){
+        if (empty($ip_address)) {
+            $client  = @$_SERVER['HTTP_CLIENT_IP'];
+            $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+            $server  = @$_SERVER['SERVER_ADDR'];
+            $remote  = @$_SERVER['REMOTE_ADDR'];
+            if(!empty($client) && filter_var($client, FILTER_VALIDATE_IP)){
+                $ip = $client;
+            }elseif(!empty($forward) && filter_var($forward, FILTER_VALIDATE_IP)){
+                $ip = $forward;
+            }elseif(!empty($server) && filter_var($server, FILTER_VALIDATE_IP)){
+                $ip = $server;   
+            }else{
+                $ip = $remote;
+            }
+        } else {
+            $ip = "$ip_address";
+        }
+        
+        $ip_data = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=".$ip));
+        $location  = "null";
+    
+        if($ip_data && $ip_data['geoplugin_countryCode'] != null){
+            $location = $ip_data['geoplugin_countryCode'];
+        }
+        return $location;
+    }
+
 }
