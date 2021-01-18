d0b78ecab2074b453bd9c38a70acbe85852a0eb4
Ana Beatriz Torrez Gomez <anitatorrez1924@gmail.com>
2021-01-18 12:11:58 -0600

    ubicacion print

--- app/Http/Controllers/FrontEnd/SubjectsController.php
+++ app/Http/Controllers/FrontEnd/SubjectsController.php
@@ -102,19 +102,13 @@ class SubjectsController extends Controller
     public function getIP(){
         if (empty($ip_address)) {
             $client  = @$_SERVER['HTTP_CLIENT_IP'];
-            $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-            $server  = @$_SERVER['SERVER_ADDR'];
             $remote  = @$_SERVER['REMOTE_ADDR'];
             if(!empty($client) && filter_var($client, FILTER_VALIDATE_IP)){
                 $ip = $client;
-            }elseif(!empty($forward) && filter_var($forward, FILTER_VALIDATE_IP)){
-                $ip = $forward;
-            }elseif(!empty($server) && filter_var($server, FILTER_VALIDATE_IP)){
-                $ip = $server;   
             }else{
                 $ip = $remote;
             }
-        } else {
+        }else {
             $ip = "$ip_address";
         }
         return $ip;
