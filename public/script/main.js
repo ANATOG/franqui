a9c5b08f319604d8a78fd18823067274655dd1b3
ANATOG <anitatorrez1924@gmail.com>
2021-04-17 19:59:53 -0600

    hh

--- public/script/main.js
+++ public/script/main.js
@@ -3912,7 +3912,7 @@
                             document.getElementById('suscripcion').style.display = 'none';
                             document.getElementsByTagName('body')[0].style.opacity = '100';
 
-                        }, 3000);
+                        }, 2000);
                         s.to(e._spinner, .5, { opacity: 0 })
                     },
                     error: function(t) {
